df41b58452d20915b8d1cdf528db9ed566fe126a
Scott Gledhill <scottgledhill@github.com>
2018-10-23 14:54:50 +0100

    component did mount adeed

--- src/Clock.js
+++ src/Clock.js
@@ -4,7 +4,19 @@ class Clock extends React.Component {
 	constructor(props) {
     super(props);
     this.state = {date: new Date()};
+	}
+	
+	componentDidMount() {
+		this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
   }
+
+  componentWillUnmount() {
+
+  }
+
 
   render() {
     return (
