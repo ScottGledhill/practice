74983353f052e44216d9cad9bb6a3c7130429a2a
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-10-27 19:45:33 +0100

    counter object passed instead of id and value

--- src/CounterComponents.js/Counter.js
+++ src/CounterComponents.js/Counter.js
@@ -6,7 +6,7 @@ class Counter extends Component {
     super(props)
     this.handleIncrement = this.handleIncrement.bind(this)
     this.state = { 
-      value: this.props.value
+      value: this.props.counter.value
   }
 }
 
@@ -19,7 +19,7 @@ class Counter extends Component {
       <div className="counterContainer">
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment </button>
-        <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2"> Delete</button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2"> Delete</button>
       </div>
     );
   }
