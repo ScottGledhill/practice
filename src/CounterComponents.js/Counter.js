84077b1482e99f91cce486653908b1a2cd846cd2
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-10-27 17:54:47 +0100

    handleicnrement added, bound

--- src/CounterComponents.js/Counter.js
+++ src/CounterComponents.js/Counter.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 
 class Counter extends Component {
+  constructor () {
+    super()
+    this.handleIncrement = this.handleIncrement.bind(this)
+  }
   state = { 
-    count: 0
+    count: 2
   }
 
-  styles = {
-    fontSize: 50,
-    fontWeight: "bold"
+  handleIncrement() {
+    console.log('increment clicked')
   }
-
+  
   render() { 
 
     return ( 
       <div>
-        <span className="{this.getBadgeClasses()}">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm"> Increment </button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment </button>
       </div>
     );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
