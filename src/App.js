024f572e6f8757803fe571106b70dba458c7e721
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-10-27 20:14:39 +0100

    component navbar created;

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Clock from './ClockComponents.js/Clock'
 import Counters from './CounterComponents.js/Counters'
+import NavBar from './NavbarComponents.js/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
+        <main classname="container"></main>
         <Clock />
         <Counters />
       </div>
