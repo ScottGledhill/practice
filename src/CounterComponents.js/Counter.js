d87b113bd7d02e26bcf547ee8110cbc1bea74e46
Scott Gledhill <scottgledhill@github.com>
2018-10-27 16:54:23 +0100

    format count added, reactstrap added

--- src/CounterComponents.js/Counter.js
+++ src/CounterComponents.js/Counter.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
 class Counter extends Component {
   state = { 
-    
+    count: 0
   }
 
   render() { 
     return ( 
-      null
-     );
+      <div>
+        <span className="badge badge-warning m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm"> Increment </button>
+      </div>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
  
+
 export default Counter;
