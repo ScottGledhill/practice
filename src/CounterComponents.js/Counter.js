118dbdbd0ebca3c829f2c8eb8d60978086a0f975
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-10-27 17:18:22 +0100

    classes added to un hardcode classname

--- src/CounterComponents.js/Counter.js
+++ src/CounterComponents.js/Counter.js
@@ -12,15 +12,18 @@ class Counter extends Component {
   }
 
   render() { 
+    let classes = "badge m-2 badge-"
+    classes += this.state.count === 0 ? "warning" : "primary"
+
     return ( 
       <div>
-        <span className="badge badge-warning m-2">{this.formatCount()}</span>
+        <span className="{classes}">{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm"> Increment </button>
       </div>
     );
   }
   formatCount() {
-    const { count } = this.state; test
+    const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
