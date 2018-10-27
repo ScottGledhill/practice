a4b10068714d7dc5edc387c293ca62e50f995b7a
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-10-27 20:06:35 +0100

    state removed from counter, handle increment added to counters to control state only here

--- src/CounterComponents.js/Counters.js
+++ src/CounterComponents.js/Counters.js
@@ -4,7 +4,6 @@ import Counter from './Counter.js'
 class Counters extends Component {
   constructor(props) {
     super(props);
-    this.handleDelete = this.handleDelete.bind()
     this.state = { 
       counters : [
         { id: 1, value: 4 },
@@ -15,16 +14,33 @@ class Counters extends Component {
     }
   }
 
-  handleDelete (counterId) {
+  handleDelete = (counterId) => {
     const counters = this.state.counters.filter(c => c.id !== counterId)
+    this.setState({ counters })
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0
+      return c
+    })
+    this.setState({ counters })
+  }
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters]
+    const index = counters.indexOf(counter)
+    counters[index] = { ...counter}
+    counters[index].value++
     this.setState({ counters })
   }
 
   render() { 
     return ( 
       <div>
+        <button onClick={this.handleReset} classname="btn btn-primary btn-sm m-2">Reset</button>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}/>
+          <Counter key={counter.id} onDelete={this.handleDelete} onIncrement={this.handleIncrement} counter={counter}/>
         ))}
       </div>
     );
